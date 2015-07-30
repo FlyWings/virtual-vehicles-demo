0152b2d7673836c807766fd691860a86347e92d8
FlyWings <wangchang517@163.com>
2015-07-30 16:33:35 +0800

    add mongodb root and admin

--- Authentication/startAuth.sh
+++ Authentication/startAuth.sh
@@ -1,5 +1,5 @@
 ## get the mongodb service host and port information using envrionement 
-echo "mongodb.uri = mongodb://$MONGODB_SERVICE_HOST:$MONGODB_SERVICE_PORT/virtual_authentication" >> /apps/config/dev/environment.properties
+echo "mongodb.uri = mongodb://admin:passw0rd@$MONGODB_SERVICE_HOST:$MONGODB_SERVICE_PORT/virtual_authentication" >> /apps/config/dev/environment.properties
 
 ## start mvn build and the application
 cd /apps
