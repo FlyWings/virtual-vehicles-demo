433ff24eb3d0dec91bbc2d202dca0e07f91a61f5
FlyWings <wangchang517@163.com>
2015-07-30 13:21:04 +0800

    cancel

--- Authentication/startAuth.sh
+++ Authentication/startAuth.sh
@@ -1,5 +1,4 @@
 ## get the mongodb service host and port information using envrionement 
-chmod -R 755 /apps
 echo "mongodb.uri = mongodb://$MONGODB_SERVICE_HOST:$MONGODB_SERVICE_PORT/virtual_authentication" >> /apps/config/dev/environment.properties
 
 ## start mvn build and the application
